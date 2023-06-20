3c5967d765ab0333e460c043ae070535ee22bc11
yuzu <kzjoe222@gmail.com>
2023-06-20 15:07:45 +0900

    ファイル構成を作成

---
+++ home/pocker/app.js
@@ -0,0 +1,7 @@
+import Game from "./main.js";
+
+// ゲームのインスタンスを作成
+const game = new Game();
+
+// ゲームを実行
+game.run();
---
+++ home/pocker/computer.js
@@ -0,0 +1,34 @@
+import Pair from './pair.js';
+import Player from './player.js';
+
+export default class Computer extends Player {
+  constructor(selector) {
+    super(selector);
+  }
+
+  // 交換するカードを選択
+  selectCard() {
+    /*
+    コンピュータの交換戦略
+    1. 役が揃っていない場合：全てのカードを交換
+    2. ワンペア or ツーペア or スリーカード：残りのカードを交換
+    3. それ以外の役が揃っている場合：交換しない
+    */
+
+    // 成立している役を調べる
+    const strength = Pair.judge(this.cards).strength;
+    // １の場合
+    if (strength === 0) {
+      this.nodes.forEach((node) => super.selectCard(node));
+    }
+    // ２の場合
+    else if (1 <= strength && strength <= 3) {
+      this.cards.forEach((card, index) => {
+        const sameRankCards = this.cards.filter((c) => c.rank === card.rank);
+        if (sameRankCards.length === 1) { // 自分以外に同じランクのカードがない場合
+          super.selectCard(this.nodes[index]); // ペアを持たないので交換
+        }
+      });
+    }
+  };
+}
