760b1aed513d4ce98221df160a213d962f74b4e4
Shogo-Imanaka <al21010@shibaura-it.ac.jp>
2023-07-07 00:36:14 +0900

    スコア表示画面からランキング画面への遷移を追加しました また、Webサーバー構築に伴いランキング登録、呼出のフェッチ先をローカルIPに変更しました

--- home/ranking/senddata.js
+++ home/ranking/senddata.js
@@ -3,10 +3,14 @@
 ***Date                 : 2023/7/2
 ***Purpose              : ランキングデータ登録処理
 **************************************************************/
-
+/*
+*** Revision :
+*** V1.0 : 今中 翔悟, 2023.07.02
+*** V1.1 : 今中 翔悟, 2023.07.06 フェッチ先をローカルサーバーに変更
+*/
 const sendData = function sendData(gameId,score){
   console.log('function called');
-  fetch('http://160.16.141.77:51280',{
+  fetch('http://127.0.0.1:3000',{
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
