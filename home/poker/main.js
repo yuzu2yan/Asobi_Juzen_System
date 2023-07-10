53942202bffb480030db50a28f29ad71e855e170
yuzu <kzjoe222@gmail.com>
2023-07-10 10:47:23 +0900

    ポーカーからスコア表示へのページ遷移を実装

--- home/poker/main.js
+++ home/poker/main.js
@@ -153,7 +153,7 @@ export default class Game {
     const youResult = Pair.judge(this.#you.cards);
     const computerResult = Pair.judge(this.#computer.cards);
     // 勝敗メッセージ
-    let message = `あなた${youResult.hand} vs 相手${computerResult.hand}\n`;
+    let message = `あなた：${youResult.hand} vs 相手：${computerResult.hand}\n`;
     // 勝者の判定
     if (youResult.strength < computerResult.strength) {
       message += `あなたの負けです\nもう一度挑戦しますか？`;
@@ -170,14 +170,10 @@ export default class Game {
         location.href = `high&low.html?score=${youResult.score}`;
       }
       else{
-        if (window.confirm(`もう一度挑戦しますか？`)) {
-          this.#initialize();
-        }
-        else {
-          location.href = "../index.html";
+          location.href = `../score_display/ScoreDisp.html?score=${youResult.score}`;
         }
       }
-    } else {
+    else {
       // 役が同じ場合は、ランクの強い方を勝者とする
       if (youResult.rank < computerResult.rank) {
         message += `あなたの負けです\nもう一度挑戦しますか？`;
@@ -194,12 +190,7 @@ export default class Game {
           location.href = `high&low.html?score=${youResult.score}`;
         }
         else {
-          if (window.confirm(`もう一度挑戦しますか？`)) {
-            this.#initialize();
-          }
-          else {
-            location.href = "../index.html";
-          }
+          location.href = `../score_display/ScoreDisp.html?score=${youResult.score}`;
         }
       } else {
         message += `引き分けです\nもう一度挑戦しますか？`;
